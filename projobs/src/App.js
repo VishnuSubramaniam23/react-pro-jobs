12dd9cc72d0ba3d027a38aa902688b00f0975696
Vishnusubramaniam23 <write2vsankar@gmail.com>
2021-11-12 12:19:40 +0530

    projobs

---
+++ projobs/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HomeComponent from './components/HomeComponent';
+
+
+function App() {
+
+  return (
+    <div>
+      <HomeComponent/>
+    </div>
+  );
+}
+
+export default App;
